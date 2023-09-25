c3768c4f4648fd77a0d38770c62e800b499c29c4
gopeshwark <gopeshwark@gmail.com>
2023-09-25 18:20:00 +0530

    Created profile settings page functionality

--- core/views.py
+++ core/views.py
@@ -13,6 +13,28 @@ def index(request):
     return render(request, 'index.html')
 
 
+@login_required(login_url="signin")
+def settings(request):
+    print(request.user)
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        bio = request.POST['bio']
+        location = request.POST['location']
+
+        if request.FILES.get('image') is not None:
+            image = request.FILES.get('image')
+            user_profile.profileimg = image
+
+        user_profile.bio = bio
+        user_profile.location = location
+        user_profile.save()
+
+        return redirect('settings')
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
+
+
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -33,12 +55,16 @@ def signup(request):
                 user.save()
 
                 # Log user in and redirect to profile seeting page
+                user_login = auth.authenticate(
+                    username=username, password=password)
+                auth.login(request, user_login)
 
                 # create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(
                     user=user_model, id_user=user_model.id)
                 new_profile.save()
+                return redirect('settings')
         else:
             messages.info(request, "Password doesn't match")
             return redirect("signup")
