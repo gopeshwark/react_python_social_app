a27c7491ded62cacc45f770c2f541cb0b36fd3e1
gopeshwark <gopeshwark@gmail.com>
2023-09-26 18:14:48 +0530

    Implemented upload post functionality

--- core/views.py
+++ core/views.py
@@ -16,6 +16,20 @@ def index(request):
     return render(request, 'index.html', {'user_profile': user_profile})
 
 
+@login_required(login_url='signin')
+def upload(request):
+
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+    return redirect("/")
+
+
 @login_required(login_url="signin")
 def settings(request):
     print(request.user)
